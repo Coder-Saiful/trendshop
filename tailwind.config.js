5fb58bbd89f06f2ee5327e0eb7068ee7e8148022
Coder-Saiful <md.syful.max970@gmail.com>
2023-10-13 00:15:17 +0600

    header part responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,21 @@ module.exports = {
       center: true
     },
     screens: {
+      'xs1': '300px',
+      // => @media (min-width: 300px) { ... }
+      'xs2': '400px',
+      // => @media (min-width: 400px) { ... }
       'sm': '576px',
-      // => @media (min-width: 640px) { ... }
+      // => @media (min-width: 576px) { ... }
 
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
       'lg': '992px',
-      // => @media (min-width: 1024px) { ... }
+      // => @media (min-width: 992px) { ... }
 
       'xl': '1200px',
-      // => @media (min-width: 1280px) { ... }
+      // => @media (min-width: 1200px) { ... }
     },
     extend: {},
   },
